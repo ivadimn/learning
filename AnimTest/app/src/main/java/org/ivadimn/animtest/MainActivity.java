3abfa5f34796ae078e3a191d13fca4024db04e96
ivadimn <ivadimn@mail.ru>
2016-07-25 21:38:37 +0300

    редакция

--- AnimTest/app/src/main/java/org/ivadimn/animtest/MainActivity.java
+++ AnimTest/app/src/main/java/org/ivadimn/animtest/MainActivity.java
@@ -1,5 +1,7 @@
 package org.ivadimn.animtest;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,14 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView image = (ImageView) findViewById(R.id.image);
+        final TextView image = (TextView) findViewById(R.id.text_id);
         final Button btnStart = (Button) findViewById(R.id.btn_start);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Animation anim = AnimationUtils.loadAnimation(
+               /* Animation anim = AnimationUtils.loadAnimation(
                         getApplicationContext(), R.anim.combination);
-                image.startAnimation(anim);
+                image.startAnimation(anim);*/
+                AnimatorSet animSet = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),
+                        R.anim.property_animator);
+                animSet.setTarget(image);
+                animSet.start();
             }
         });
     }
