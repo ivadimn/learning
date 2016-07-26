bfac40a4b5b280d2afd996a4df28fbc100226121
ivadimn <ivadimn@mail.ru>
2016-07-26 21:10:51 +0300

    ИЗМЕНЕНИЯ

--- AnimTest/app/src/main/java/org/ivadimn/animtest/MainActivity.java
+++ AnimTest/app/src/main/java/org/ivadimn/animtest/MainActivity.java
@@ -2,6 +2,8 @@ package org.ivadimn.animtest;
 
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.TimeInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,6 +28,16 @@ public class MainActivity extends AppCompatActivity {
                /* Animation anim = AnimationUtils.loadAnimation(
                         getApplicationContext(), R.anim.combination);
                 image.startAnimation(anim);*/
+                TimeInterpolator ti = new TimeInterpolator() {
+                    @Override
+                    public float getInterpolation(float v) {
+                        return 0;
+                    }
+                }
+                ObjectAnimator anim = ObjectAnimator.ofFloat(image, "rotation", 0f, 90f);
+                anim.setDuration(2000);                  // Duration in milliseconds
+                anim.setInterpolator(TimeInterpolator);  // E.g. Linear, Accelerate, Decelerate
+                anim.start()
                 AnimatorSet animSet = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),
                         R.anim.property_animator);
                 animSet.setTarget(image);
