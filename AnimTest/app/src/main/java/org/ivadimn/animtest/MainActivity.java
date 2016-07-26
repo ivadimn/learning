4388093d67a4c1a567b9c3d5e2e621481036589f
ivadimn <ivadimn@mail.ru>
2016-07-26 21:37:36 +0300

    изменения

--- AnimTest/app/src/main/java/org/ivadimn/animtest/MainActivity.java
+++ AnimTest/app/src/main/java/org/ivadimn/animtest/MainActivity.java
@@ -20,29 +20,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView image = (TextView) findViewById(R.id.text_id);
-        final Button btnStart = (Button) findViewById(R.id.btn_start);
 
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-               /* Animation anim = AnimationUtils.loadAnimation(
-                        getApplicationContext(), R.anim.combination);
-                image.startAnimation(anim);*/
-                TimeInterpolator ti = new TimeInterpolator() {
-                    @Override
-                    public float getInterpolation(float v) {
-                        return 0;
-                    }
-                }
-                ObjectAnimator anim = ObjectAnimator.ofFloat(image, "rotation", 0f, 90f);
-                anim.setDuration(2000);                  // Duration in milliseconds
-                anim.setInterpolator(TimeInterpolator);  // E.g. Linear, Accelerate, Decelerate
-                anim.start()
-                AnimatorSet animSet = (AnimatorSet) AnimatorInflater.loadAnimator(getApplicationContext(),
-                        R.anim.property_animator);
-                animSet.setTarget(image);
-                animSet.start();
-            }
-        });
     }
 }
