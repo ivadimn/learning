4f91b9d79cf7edad4775aa6b2be3749732aec766
ivadimn <ivadimn@mail.ru>
2016-07-27 18:15:54 +0300

    изменения

--- AnimTest/app/src/main/java/org/ivadimn/animtest/MainActivity.java
+++ AnimTest/app/src/main/java/org/ivadimn/animtest/MainActivity.java
@@ -6,6 +6,8 @@ import android.animation.ObjectAnimator;
 import android.animation.TimeInterpolator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -15,11 +17,35 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView1);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id  = item.getItemId();
+        switch(id) {
+            case R.id.mi_scale_id:
+                scaleAnimation();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void scaleAnimation() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.scale);
+        textView.startAnimation(anim);
 
     }
 }
