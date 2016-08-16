a23b477acfe6b304f406322505a82e59dc465174
ivadimn <ivadimn@mail.ru>
2016-08-16 19:10:29 +0300

    обновлено

--- Experiment/app/src/main/java/org/ivadimn/experiment/SimpleDialog.java
+++ Experiment/app/src/main/java/org/ivadimn/experiment/SimpleDialog.java
@@ -1,5 +1,6 @@
 package org.ivadimn.experiment;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDialog;
@@ -17,7 +18,8 @@ public class SimpleDialog extends AppCompatDialog {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog);
-        setTitle("Dialog");
+        setTitle("");
+
 
     }
 
