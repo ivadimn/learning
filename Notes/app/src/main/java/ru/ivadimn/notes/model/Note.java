51452f78f1d2f0b1dd4b0f7995e45f408e945e30
ivadimn <ivadimn@mail.ru>
2016-10-14 10:19:20 +0300

    new version

--- Notes/app/src/main/java/ru/ivadimn/notes/model/Note.java
+++ Notes/app/src/main/java/ru/ivadimn/notes/model/Note.java
@@ -1,10 +1,23 @@
 package ru.ivadimn.notes.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by vadim on 12.10.16.
  */
 
 public class Note implements Serializable {
+
+    private String title;
+    private String content;
+    private Date moment;
+
+    public Note(String title, String content, Date moment) {
+        this.title = title;
+        this.content = content;
+        this.moment = moment;
+    }
+
+
 }
