e0bc0283c8fa6d5e5bab23e7ade01865377c6183
ivadimn <ivadimn@mail.ru>
2016-11-02 08:29:16 +0300

    new version

---
+++ Notes/app/src/main/java/ru/ivadimn/notes/common/Utils.java
@@ -0,0 +1,15 @@
+package ru.ivadimn.notes.common;
+
+import ru.ivadimn.notes.model.Note;
+
+/**
+ * Created by vadim on 21.10.16.
+ */
+
+public class Utils {
+
+    public static void writeNotes(Note [] notes) {
+
+    }
+
+}
--- Notes/app/src/main/java/ru/ivadimn/notes/ui/MainActivity.java
+++ Notes/app/src/main/java/ru/ivadimn/notes/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package ru.ivadimn.notes;
+package ru.ivadimn.notes.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ru.ivadimn.notes.R;
 
 public class MainActivity extends AppCompatActivity {
 
