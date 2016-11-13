dbb28d654ea8851107f9aa4c2bb02e4036ff3534
ivadimn <ivadimn@mail.ru>
2016-11-13 12:47:03 +0300

    что-то поменялось

--- Notes/app/src/main/java/ru/ivadimn/notes/common/Utils.java
+++ Notes/app/src/main/java/ru/ivadimn/notes/common/Utils.java
@@ -1,6 +1,10 @@
 package ru.ivadimn.notes.common;
 
+import android.content.SharedPreferences;
+
 import ru.ivadimn.notes.model.Note;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by vadim on 21.10.16.
@@ -8,6 +12,7 @@ import ru.ivadimn.notes.model.Note;
 
 public class Utils {
 
+    public static final String PACKAGE = "ru.ivadimn.notes";
     public static void writeNotes(Note [] notes) {
 
     }
