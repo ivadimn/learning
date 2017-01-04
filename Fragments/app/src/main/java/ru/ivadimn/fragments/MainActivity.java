022bf2fd904a7760ca14db6d43f12c190ad126b3
ivadimn <ivadimn@mail.ru>
2017-01-04 10:12:01 +0300

    new project

--- Fragments/app/src/main/java/ru/ivadimn/fragments/MainActivity.java
+++ Fragments/app/src/main/java/ru/ivadimn/fragments/MainActivity.java
@@ -2,12 +2,47 @@ package ru.ivadimn.fragments;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Bundle bundle;
+    private boolean isFirstFragment=true;
+
+    private static String FRAGMENT_A="FragmentA";
+    private static String FRAGMENT_B="FragmentB";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle("");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
