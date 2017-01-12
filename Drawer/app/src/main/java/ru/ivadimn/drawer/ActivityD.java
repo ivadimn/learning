e0fda3ebf8dbf6ab42ccf9bfee8f560699f2be80
ivadimn <ivadimn@mail.ru>
2017-01-12 17:58:51 +0300

    version

--- Drawer/app/src/main/java/ru/ivadimn/drawer/ActivityD.java
+++ Drawer/app/src/main/java/ru/ivadimn/drawer/ActivityD.java
@@ -1,10 +1,13 @@
 package ru.ivadimn.drawer;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class ActivityD extends AppCompatActivity {
 
@@ -17,12 +20,31 @@ public class ActivityD extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar_id);
         setSupportActionBar(toolbar);
-        toolbar.setTitle("Заголовок");
+        getSupportActionBar().setTitle("Заголовок");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.fragment_two, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_item_add_id:
+                showFragment(new FragmentList());
+                break;
+            case R.id.menu_item_edit_id:
+                showFragment(new FragmentTwo());
+                break;
+        }
+        return true;
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentTransaction ft  = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, fragment);
+        ft.commit();
     }
 }
