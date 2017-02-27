3f9c56e2bad731eff0003eb45c53accf9927e7b6
ivadimn <ivadimn@mail.ru>
2017-02-27 23:33:40 +0300

    edition

--- Notes/app/src/main/java/ru/ivadimn/notes/ui/activities/MainActivity.java
+++ Notes/app/src/main/java/ru/ivadimn/notes/ui/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package ru.ivadimn.notes.ui;
+package ru.ivadimn.notes.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ Notes/app/src/main/java/ru/ivadimn/notes/ui/adapters/NotesAdapter.java
@@ -0,0 +1,8 @@
+package ru.ivadimn.notes.ui.adapters;
+
+/**
+ * Created by vadim on 27.02.2017.
+ */
+
+public class NotesAdapter  {
+}
