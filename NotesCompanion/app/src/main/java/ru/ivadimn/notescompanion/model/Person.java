8dc62e44549b96dadc37013bad02249dfb9bcf5b
ivadimn <ivadimn@mail.ru>
2017-05-02 21:32:52 +0300

    new version

--- NotesCompanion/app/src/main/java/ru/ivadimn/notescompanion/model/Person.java
+++ NotesCompanion/app/src/main/java/ru/ivadimn/notescompanion/model/Person.java
@@ -1,13 +1,27 @@
 package ru.ivadimn.notescompanion.model;
 
+import android.net.Uri;
+import android.provider.ContactsContract;
+
 /**
  * Created by vadim on 27.04.17.
  */
 
 public class Person  {
 
+    Uri CONTACT_URI = ContactsContract.Contacts.CONTENT_URI;
+    public static final String _ID = ContactsContract.Contacts._ID;
+    public static final String PHOTO_ID = ContactsContract.Contacts.PHOTO_ID;
+    public static final String PHOTO_FILE_ID = ContactsContract.Contacts.PHOTO_FILE_ID;
+    public static final String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+    public static final String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+    Uri NUMBER_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+    public static final String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+
     private String name;
-    private String phone;
+    private String[] phones;
     private int photo;
 
     public Person() {
