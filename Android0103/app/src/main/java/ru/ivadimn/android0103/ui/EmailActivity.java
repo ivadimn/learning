591cacba5bffa0d37fbb88d93a62eac5caef7ad1
ivadimn <ivadimn@mail.ru>
2017-05-31 17:56:52 +0300

    new project

---
+++ Android0103/app/src/main/java/ru/ivadimn/android0103/ui/EmailActivity.java
@@ -0,0 +1,20 @@
+package ru.ivadimn.android0103.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import ru.ivadimn.android0103.R;
+
+/**
+ * Created by vadim on 31.05.17.
+ */
+
+public class EmailActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email);
+    }
+}
---
+++ Android0103/app/src/main/java/ru/ivadimn/android0103/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.ivadimn.android0103.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ru.ivadimn.android0103.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
