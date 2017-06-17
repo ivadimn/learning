45dbc6b4d34c0dad8effe1298a7667dcc0333cbb
ivadimn <ivadimn@mail.ru>
2017-06-17 21:45:17 +0300

    new project

---
+++ Android0108/app/src/main/java/ru/ivadimn/android0108/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.ivadimn.android0108;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
